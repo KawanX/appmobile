435160dcecee7c33b07266405b47159737609600
Priscila Costa <Kawan Ximendes>
2021-10-23 18:14:51 -0300

    app completo

---
+++ www/js/firebase.js
@@ -0,0 +1,13 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+    apiKey: "AIzaSyDNqQaqPmcfBp2_3Uxu15dM9B8CBhQtbRQ",
+    authDomain: "aulapab-c0d77.firebaseapp.com",
+    projectId: "aulapab-c0d77",
+    storageBucket: "aulapab-c0d77.appspot.com",
+    messagingSenderId: "1069595034189",
+    appId: "1:1069595034189:web:ae74760039addf72c6d204",
+    measurementId: "G-HKJKL6THTT"
+ };
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
